eb5f9a62f9e3d9be5874cf2c2e09c102a61cb914
aguremoto <aguremoto@MBP-de-Agustin.home>
2020-02-10 19:18:35 +0100

    se agrego  ejemplos de codigo v8

--- ejemplos/1_groovy_connect_and_list_s3.groovy
+++ ejemplos/1_groovy_connect_and_list_s3.groovy
@@ -6,17 +6,14 @@ node {
            print 'Hello, world !'
            if (i==0)
            {
-
             withCredentials([[
             $class: 'AmazonWebServicesCredentialsBinding', 
             credentialsId: 'aws-jenkins-server', 
             accessKeyVariable: 'AWS_ACCESS_KEY_ID', 
             secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
             ]]) 
-            {
             sh 'aws --version' 
             sh 'aws s3 ls'
-            } 
            }
            else {
                build 'Declarative pipeline'
