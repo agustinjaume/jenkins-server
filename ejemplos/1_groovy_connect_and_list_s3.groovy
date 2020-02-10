02c595aea606de97f7dd2257139aa61f2216d5a5
aguremoto <aguremoto@MBP-de-Agustin.home>
2020-02-10 19:13:07 +0100

    se agrego  ejemplos de codigo v5

--- ejemplos/1_groovy_connect_and_list_s3.groovy
+++ ejemplos/1_groovy_connect_and_list_s3.groovy
@@ -7,16 +7,17 @@ node {
            if (i==0)
            {
 
-      steps { withCredentials([[
-        $class: 'AmazonWebServicesCredentialsBinding', 
-        credentialsId: 'aws-jenkins-server', 
-        accessKeyVariable: 'AWS_ACCESS_KEY_ID', 
-        secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
-        ]]) 
-        {
-        aws --version 
-        aws s3 ls
-        } }
+            step { withCredentials([[
+            $class: 'AmazonWebServicesCredentialsBinding', 
+            credentialsId: 'aws-jenkins-server', 
+            accessKeyVariable: 'AWS_ACCESS_KEY_ID', 
+            secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+            ]]) 
+            {
+            aws --version 
+            aws s3 ls
+            } 
+            }  // fin step
 
            }
            else {
